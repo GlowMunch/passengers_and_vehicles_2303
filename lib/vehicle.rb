eee1b458da27f996ded5283ec20caeaab9555588
Blaine Glasgow <glasgowb@outlook.com>
2023-04-05 08:39:43 -0700

    Add methods to vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,6 +1,23 @@
 class Vehicle
-  attr_reader
-  def initialize
+  attr_reader :year, :make, :model, :passengers
+  def initialize (year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @speeding = false
+    @passengers = []
+  end
+
+  def speed
+    @speeding = true
+  end
+
+  def add_passenger(ryder)
+    @passengers << ryder
+  end
+
+  def num_adults
     
   end
+
 end
